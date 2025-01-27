93d165e9ed3f3f719f5c0c1d0fdc8504dc765e76
Saim Islam <saim.islam698@gmail.com>
2025-01-27 03:03:21 +0000

    Changes from pendrive to ubuntu to github

--- Task_00.c
+++ Task_00.c
@@ -2,13 +2,13 @@
 
 int main(){
      double a,b,sum,product;
-     print("Enter two numbers separated by commas: \n");
+     printf("Enter two numbers separated by space: \n");
      scanf("%lf %lf", &a, &b);
      sum = a+b;
      product = a*b;
 
-     print("\nSum: %lf", sum);
-     print("\nProduct: %lf", product);
+     printf("\nSum: %lf", sum);
+     printf("\nProduct: %lf\n", product);
 
      return 0;
 }
